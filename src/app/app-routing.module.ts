6d6082c75ac9b7a5e87baa272befe68162b417d2
dinnerhe <dinnerhe@hotmail.com>
2022-09-20 17:49:37 -0700

    Created two pages& setup routing(no content added yet)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent} from './component/about-me/about-me.component';
+import { MainPageComponent} from "./component/main-page/main-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'about-me', component: AboutMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './component/about-me/about-me.component';
+import { MainPageComponent } from './component/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
